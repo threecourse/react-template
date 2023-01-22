e2c28a837179c21ca96e59f860db8e83e49710c4
threecourse <daisukeka@gmail.com>
2023-01-22 23:47:15 +0900

    vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [
     react(),
     tsconfigPaths()
-  ]
+  ],
 })
